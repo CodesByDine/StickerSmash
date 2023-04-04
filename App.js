3f6daf3b680e344beae4c6a14dbc6f3cd29bbd3a
Deen <gingerg238@gmail.com>
2023-04-04 12:04:56 +0200

    Create button

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import Button from "./Components/Button";
+import ImageViewer from "./Components/ImageViewer";
+
+const PlaceholderImage = require("./assets/images/background-image.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#fff" }}>
-        Open up App.js to start working on your app!
-      </Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,6 +26,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  // Styles that are unchanged from previous step are hidden for brevity.
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
